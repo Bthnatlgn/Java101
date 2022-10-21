788c9875df6c645105df8c5d71a4def1a437b616
Batuhan <bthnatlgn54@gmail.com>
2022-10-21 23:29:29 +0300

    SayiSaydırma
    Java101

---
+++ loopDemo/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //for döngüsü
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
+        System.out.println("For Döngüsü Bitti");
+        //while döngüsü
+        int i = 1;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+
+        }
+        System.out.println("While Döngüsü Bitti");
+    }
+}
