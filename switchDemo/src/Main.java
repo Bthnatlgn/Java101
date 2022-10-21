0e6098ec7db0f5691af9514d586f85a384d68711
Batuhan <bthnatlgn54@gmail.com>
2022-10-21 23:23:49 +0300

    UniversiteNotHesaplama
    Java101

---
+++ switchDemo/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        char grade = 'M';
+
+        switch (grade) {
+
+            case 'A':
+                System.out.println("Mükemmel : Geçtiniz");
+                break;
+
+            case 'B':
+                System.out.println("Çok Güzel : Geçtiniz");
+                break;
+            case 'C':
+                System.out.println("İyi : Geçtiniz");
+                break;
+            case 'D':
+                System.out.println("İdare Eder : Geçtiniz");
+                break;
+            case 'F':
+                System.out.println("Malesef : Kaldınız");
+                break;
+            default:
+                System.out.println("Geçersiz Not Girdiniz");
+
+        }
+
+    }
+}
